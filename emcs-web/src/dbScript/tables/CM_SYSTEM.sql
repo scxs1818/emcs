ba217a7d89eda3a7f95e1a358640ebf950e6dcd1
vir <3082612539@qq.com>
2018-02-06 20:46:09 +0800

    唐强

---
+++ emcs-web/src/dbScript/tables/CM_SYSTEM.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS  `CM_SYSTEM`;
+CREATE TABLE `CM_SYSTEM` (
+   `RUN_DATE`             DATE                           NOT NULL COMMENT '系统日期',
+   `PREV_RUN_DATE`        DATE                           NOT NULL COMMENT '前一工作日',
+   `CORE_RUN_DATE`        DATE                             COMMENT '三方日期',
+   `SER_STATUS_FLAG`	VARCHAR(2)		       NOT NULL COMMENT '服务状态标志:N-关闭,Y-开启'
+);
