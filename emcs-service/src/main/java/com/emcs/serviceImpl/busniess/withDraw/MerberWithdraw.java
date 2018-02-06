6612768d8462936e52f473bb465cc5950afece0c
lll-666 <35371053+lll-666@users.noreply.github.com>
2018-02-06 13:24:54 +0800

    tangqiang

--- emcs-service/src/main/java/com/emcs/serviceImpl/busniess/withDraw/MerberWithdraw.java
+++ emcs-service/src/main/java/com/emcs/serviceImpl/busniess/withDraw/MerberWithdraw.java
@@ -1,17 +1,19 @@
 package com.emcs.serviceImpl.busniess.withDraw;
 
-import com.emcs.Constant.BusiConstant;
+import com.emcs.Constant.BusiConstant.*;
 import com.emcs.Super.ServiceTransactionalY;
 import com.emcs.exception.BusiException;
 import com.emcs.serviceImpl.busniess.recharge.CustRecharge;
 import com.emcs.serviceImpl.busniess.recharge.MerchRecharge;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
 /**
  * Created by Administrator on 2018/2/5.
  */
+@Service
 public class MerberWithdraw extends ServiceTransactionalY{
 
     @Autowired
@@ -25,10 +27,10 @@ public class MerberWithdraw extends ServiceTransactionalY{
         param.put("acct_status","N");
         if(oneSelect.selectIsExistVaPlatInfo(param)==0)throw new BusiException("交易平台不存在或者处于异常状态","600003");
 
-        if(BusiConstant.ROLE_CUST.equals(param.get("role_type"))){
+        if(Role.CUST.vaue().equals(param.get("role_type"))){
             param.put("cust_id",param.get("merber_id"));
             custRecharge.process(param);
-        }else if(BusiConstant.ROLE_MERCH.equals(param.get("role_type"))){
+        }else if(Role.MERCH.vaue().equals(param.get("role_type"))){
             param.put("merch_id",param.get("merber_id"));
             merchRecharge.process(param);
         }else{
