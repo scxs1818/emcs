37cd94d26dfbdb1e4303f71d0e70d872e0c3e8fb
lll-666 <35371053+lll-666@users.noreply.github.com>
2018-02-07 13:40:20 +0800

    tangqiang

--- emcs-web/dbScript/tables/CM_SYSTEM_系统表.sql
+++ emcs-web/dbScript/tables/CM_SYSTEM_系统表.sql
@@ -4,4 +4,4 @@ CREATE TABLE `CM_SYSTEM` (
    `PREV_RUN_DATE`        DATE                           NOT NULL COMMENT '前一工作日',
    `CORE_RUN_DATE`        DATE                             COMMENT '三方日期',
    `SER_STATUS_FLAG`	VARCHAR(2)		       NOT NULL COMMENT '服务状态标志:N-关闭,Y-开启'
-);
+)COMMENT '系统表';
