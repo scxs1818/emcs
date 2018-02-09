a2224483f06efa85bac560f6e5abf11a7ae0ff19
lll-666 <35371053+lll-666@users.noreply.github.com>
2018-02-09 15:37:21 +0800

    tangqiang

--- emcs-web/dbScript/tables/CM_TRAN_SEQ_交易记录表.sql
+++ emcs-web/dbScript/tables/CM_TRAN_SEQ_交易记录表.sql
@@ -1,11 +1,11 @@
 DROP TABLE  IF EXISTS CM_TRAN_SEQ ;
 CREATE TABLE CM_TRAN_SEQ
 (
-  PUB_SEQ_NO           INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT '交易公共流水号' ,
-  ORDER_NO           varchar(32) COMMENT '订单号' ,
+  PUB_SEQ_NO           VARCHAR (32) COMMENT '交易公共流水号' ,
+  ORDER_NO           VARCHAR(32) COMMENT '订单号' ,
   TRAN_DATE           DATE                      NOT NULL COMMENT '交易日期',
   TRAN_TIME           VARCHAR(10) COMMENT '交易时间',
-  CHANNEL_SEQ_NO   varchar(32) COMMENT '渠道流水号' ,
+  CHANNEL_SEQ_NO   VARCHAR(32) COMMENT '渠道流水号' ,
   CHANNEL_DATE          DATE COMMENT '渠道日期' ,
   CHANNEL_CODE        VARCHAR(20) COMMENT '渠道关键编码' ,
   SOURCE_TYPE         VARCHAR(16) COMMENT '渠道类型代码',
@@ -23,5 +23,6 @@ CREATE TABLE CM_TRAN_SEQ
   MSG_CODE            VARCHAR(10) COMMENT 'MBSD报文编码',
   TRAN_STATUS         VARCHAR(2)          NOT NULL COMMENT '交易状态代码:01-成功,02-失败',
   RET_CODE            VARCHAR(16) COMMENT '响应码',
-  FAIL_REASON         VARCHAR(512) COMMENT '失败原因'
+  FAIL_REASON         VARCHAR(512) COMMENT '失败原因',
+   PRIMARY KEY(PUB_SEQ_NO)
 ) COMMENT '交易流水记录表';
